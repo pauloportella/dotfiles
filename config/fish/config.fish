2420d589b652a9cfb8915ea1726aefabede38e1b
jrbaron <jrbaron@annas-air.home>
2021-06-25 15:29:18 +0200

    macbook-air m1 config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,16 +8,7 @@ nvm use default --silent
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
 
-# tabtab source for serverless package
-# uninstall by removing these lines or running `tabtab uninstall serverless`
-[ -f /Users/paulo.portella/dev/pauloportella/bundlewatch-service/node_modules/tabtab/.completions/serverless.fish ]; and . /Users/paulo.portella/dev/pauloportella/bundlewatch-service/node_modules/tabtab/.completions/serverless.fish
-# tabtab source for sls package
-# uninstall by removing these lines or running `tabtab uninstall sls`
-[ -f /Users/paulo.portella/dev/pauloportella/bundlewatch-service/node_modules/tabtab/.completions/sls.fish ]; and . /Users/paulo.portella/dev/pauloportella/bundlewatch-service/node_modules/tabtab/.completions/sls.fish
-# tabtab source for slss package
-# uninstall by removing these lines or running `tabtab uninstall slss`
-[ -f /Users/paulo.portella/dev/pauloportella/bundlewatch-service/node_modules/tabtab/.completions/slss.fish ]; and . /Users/paulo.portella/dev/pauloportella/bundlewatch-service/node_modules/tabtab/.completions/slss.fish
-export PATH="$PATH:/Users/paulo.portella/.kenv/bin"
-export PATH="$PATH:/Users/paulo.portella/.kit/bin"
+
+export PATH="/opt/homebrew/opt/python/libexec/bin:$PATH"
