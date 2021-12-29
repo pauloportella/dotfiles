552f647fcb3caccf007480e85053bf9cb4a87c49
Paulo Portella <hello@pauloportella.dev>
2021-12-29 17:07:03 +0000

    add libpq bin to path

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,6 +9,7 @@ nvm use default --silent
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/__tabtab.fish ]; and . ~/.config/tabtab/__tabtab.fish; or true
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_add_path "/usr/local/opt/libpq/bin" $fish_user_paths 
 
 # tabtab source for serverless package
 # uninstall by removing these lines or running `tabtab uninstall serverless`
@@ -21,3 +22,5 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 [ -f /Users/paulo.portella/dev/pauloportella/bundlewatch-service/node_modules/tabtab/.completions/slss.fish ]; and . /Users/paulo.portella/dev/pauloportella/bundlewatch-service/node_modules/tabtab/.completions/slss.fish
 export PATH="$PATH:/Users/paulo.portella/.kenv/bin"
 export PATH="$PATH:/Users/paulo.portella/.kit/bin"
+
+
