3318c76fb10ce65ea452c3bbed120e0d4bbda0e3
Paulo Portella <hello@pauloportella.dev>
2021-12-30 11:19:09 +0000

    remove unused alias

---
+++ config/fish/functions/lw.fish
@@ -0,0 +1,4 @@
+# Defined via `source`
+function lw --wraps='dev:lifeworks ; nvm use default' --description 'alias lw=dev:lifeworks ; nvm use default'
+  dev:lifeworks ; nvm use default $argv; 
+end
